ba62450a23f0a157b1ebe8055285475e8b1fab76
Exanoff12 <91356410+Exanoff12@users.noreply.github.com>
2022-05-09 21:00:50 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,29 @@
-
+cd ~
+pkg update
+pkg upgrade
+pkg install wget git yarn
+wget -O termuxblack.sh https://github.com/Hax4us/TermuxBlack/raw/master/install.sh
+wget -O apkmod.sh https://raw.githubusercontent.com/Hax4us/Apkmod/master/setup.sh
+bash termuxblack.sh -i
+apt update
+apt upgrade
+apt install lemon
+dpkg --configure -a
+bash apkmod.sh -i
+apt update
+apt upgrade
+apt install lemon
+cd /data/data/com.termux/cache/apt/archives/
+ls -a
+dpkg-dev -R lemon*.deb lemon
+cd lemon/
+cd data/data/com.termux/files/usr/
+cd bin/
+cp -r lemon $PREFIX/bin/
+cd ../
+cd share/
+cp -r lemon $PREFIX/share/
+cp -r doc $PREFIX/share/
+cd ~
+cd $PREFIX/share/lemon/
+yarn add expressjs
