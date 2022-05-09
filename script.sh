4f2d7c6ca3543e8d9d96b3cbe87fed0d1fa57b42
AKPR2007 <88362716+AKPR2007@users.noreply.github.com>
2022-05-09 22:18:01 +0600

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,18 +1,18 @@
 cd ~
-pkg update
-pkg upgrade
-pkg install wget git yarn
+yes | pkg update
+yes | pkg upgrade
+yes | pkg install wget git yarn
 wget -O termuxblack.sh https://github.com/Hax4us/TermuxBlack/raw/master/install.sh
 wget -O apkmod.sh https://raw.githubusercontent.com/Hax4us/Apkmod/master/setup.sh
 bash termuxblack.sh -i
-apt update
-apt upgrade
-apt install lemon
+yes | apt update
+yes | apt upgrade
+yes | apt install lemon
 dpkg --configure -a
 bash apkmod.sh -i
-apt update
-apt upgrade
-apt install lemon
+yes | apt update
+yes | apt upgrade
+yes | apt install lemon
 cd /data/data/com.termux/cache/apt/archives/
 ls -a
 dpkg-dev -R lemon*.deb lemon
@@ -27,3 +27,4 @@ cp -r doc $PREFIX/share/
 cd ~
 cd $PREFIX/share/lemon/
 yarn add expressjs
+cd ~
