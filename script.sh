8d34e841bd15101eaa137f5f21e36d5b4cb02184
AKPR2007 <88362716+AKPR2007@users.noreply.github.com>
2022-05-13 15:50:29 +0600

    v1.1

--- script.sh
+++ script.sh
@@ -1,11 +1,11 @@
-echo "lemon termux v1"
+echo "lemon termux v1.1"
 cd ~
 yes | pkg update
 yes | pkg upgrade
 apt install nodejs git yarn wget
 npm install -g npm@6.14.10
 wget -O apkmod_installer.sh "https://raw.githubusercontent.com/Hax4us/Apkmod/master/setup.sh"
-wget -O lemon_all-arch.deb "https://github.com/AKPR2007/l3mon-termux/blob/main/lemon_all-arch.deb?raw=true"
+wget -O lemon_all-arch.deb "https://raw.githubusercontent.com/AKPR2007/l3mon-termux/main/lemon_all-arch.deb"
 bash apkmod_installer.sh -i
 yes | apt update
 yes | apt upgrade
